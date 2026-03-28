89dff09e857aa1a81b10e4d76f428b0305922a6e
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-03-28 13:09:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "nodepool1"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2s_v2"
   }
 
   identity {
