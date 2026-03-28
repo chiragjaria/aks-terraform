b320586a080496ecba2f0c54d8bddc790370b646
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-03-28 12:01:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
 
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = var.location
+  resource_group_name = var.resource_group
+  dns_prefix          = var.aks_name
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
